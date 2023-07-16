3a6bef5a255b276f921e6ce8eb561f16ba0b1d55
Brexblime <jaouhari.bahaadine@gmail.com>
2023-07-16 21:47:50 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,12 +3,29 @@
 file storage
 """
 
+
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
     """filestorage class"""
+
+    CLASSES = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "Place": Place,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Review": Review
+    }
 
     __file_path = "file.json"
     __objects = {}
