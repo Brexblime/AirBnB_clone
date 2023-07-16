f1e5d0abafea2968beadaea884642b1a8a4cfa6b
Brexblime <jaouhari.bahaadine@gmail.com>
2023-07-16 19:16:21 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Create unique FileStroage instance"""
+from models.engine.file_storage import FileStorage
 
-from models.base_model import BaseModel
+storage = FileStorage()
+storage.reload()
