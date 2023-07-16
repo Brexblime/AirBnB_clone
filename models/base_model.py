f34c64b0beffe048b547e6c9dc9e10430b862517
Brexblime <jaouhari.bahaadine@gmail.com>
2023-07-16 16:46:37 +0100

    adding

--- models/base_model.py
+++ models/base_model.py
@@ -7,21 +7,32 @@ from datetime import datetime
 class BaseModel:
     """Base class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializing"""
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, value)
+    else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """str"""
+
         return ("[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
+        """saving"""
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """to dict"""
         data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -39,6 +39,24 @@ class TestBaseModel(unittest.TestCase):
             self.assertEqual(data['created_at'], obj.created_at.isoformat())
             self.assertEqual(data['updated_at'], obj.updated_at.isoformat())
 
+class TestBaseModelDict(unittest.TestCase):
+    """basemodeldict class"""
+
+    def test_from_dict(self):
+        """define test_from_dict"""
+
+        obj = BaseModel()
+        obj.name = "My_First_Model"
+        obj.my_number = 89
+        obj_json = obj.to_dict()
+
+        new_obj = BaseModel(**obj_json)
+        self.assertEqual(obj.id, new_obj.id)
+        self.assertEqual(obj.created_at, new_obj.created_at)
+        self.assertEqual(obj.updated_at, new_obj.updated_at)
+        self.assertEqual(obj.name, new_obj.name)
+        self.assertEqual(obj.my_number, new_obj.my_number)
+        self.assertEqual(str(obj), str(new_obj))
 
 if __name__ == '__main__':
     unittest.main()
